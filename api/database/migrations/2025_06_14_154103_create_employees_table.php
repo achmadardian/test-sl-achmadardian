18d9e92449f8be2872eb935891d66270a014e4e8
achmadardian <achmadardian37@gmail.com>
2025-06-15 00:44:27 +0700

    chore(migration): add create_employees_table

---
+++ api/database/migrations/2025_06_14_154103_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Position;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
+            $table->timestamp('hired_at')->useCurrent();
+            $table->timestamp('ended_at')->nullable();
+            $table->foreignIdFor(Position::class)->constrained();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
