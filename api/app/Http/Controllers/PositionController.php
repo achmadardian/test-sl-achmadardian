92fcf9c06e57844cab829e1ab46f0915c9e34406
achmadardian <achmadardian37@gmail.com>
2025-06-14 22:32:14 +0700

    feat: add Position Controller

---
+++ api/app/Http/Controllers/PositionController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreatePositionRequest;
+use App\Http\Requests\PaginationRequest;
+use App\Http\Resources\PositionResource;
+use App\Services\PositionService;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+
+class PositionController extends Controller
+{
+    use ApiResponse;
+
+    /**
+     * @var PositionService
+     */
+    protected PositionService $positionService;
+
+    /**
+     * @param PositionService $positionService
+     */
+    public function __construct(PositionService $positionService)
+    {
+        $this->positionService = $positionService;
+    }
+
+    /**
+     * @param PaginationRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function index(PaginationRequest $request): JsonResponse
+    {
+        $request->validated();
+        $perPage = $request->input('per_page', 10);
+        $search = $request->input('search', null);
+
+        $positions = $this->positionService->getAll(perPage: $perPage, search: $search);
+        $dto = PositionResource::collection($positions);
+        
+        return $this->success($dto, paginator: $positions);
+    }
+
+    /**
+     * @param CreatePositionRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function store(CreatePositionRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $newPosition = $this->positionService->create($data);
+        $dto = PositionResource::make($newPosition);
+
+        return $this->created($dto);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
+    {
+        try {
+            $position = $this->positionService->getById($id);
+            $dto = PositionResource::make($position);
+            
+            return $this->success($dto);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException) {
+            return $this->notFound();
+        }
+    }
+
+    /**
+     * @param CreatePositionRequest $request
+     * @param int $id
+     *
+     * @return JsonResponse
+     */
+    public function update(CreatePositionRequest $request, int $id): JsonResponse
+    {
+       try {
+            $data = $request->validated();
+            $updatePosition = $this->positionService->updateById($data, $id);
+            $dto = PositionResource::make($updatePosition);
+
+            return $this->updated($dto);
+       } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $th) {
+            return $this->notFound();
+       }
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        try {
+            $this->positionService->deleteById($id);
+            
+            return $this->deleted();
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException) {
+            return $this->notFound();
+        }
+    }
+}
