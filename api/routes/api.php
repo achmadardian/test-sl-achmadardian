339630789f75dd6fe918a132145f1f7f04c7d940
achmadardian <achmadardian37@gmail.com>
2025-06-14 22:32:33 +0700

    feat: add new Route

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PositionController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function() {
+    // positions
+    Route::apiResource('positions', PositionController::class);
+});
