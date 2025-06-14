1ab19231e2e4736891ece116e450b233e93de0b8
achmadardian <achmadardian37@gmail.com>
2025-06-14 22:28:56 +0700

    feat: add Position Model

---
+++ api/app/Models/Position.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Position extends Model
+{
+    use SoftDeletes;
+    
+    /**
+     * @var string
+     */
+    protected $table = 'positions';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name'];
+}
