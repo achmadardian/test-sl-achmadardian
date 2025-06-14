51e8be6498603cb17eacf8cff4fce28864a763b8
achmadardian <achmadardian37@gmail.com>
2025-06-14 22:30:25 +0700

    feat: add PaginationRequest

---
+++ api/app/Http/Requests/PaginationRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaginationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'per_page' => 'nullable|int|max:100',
+            'search' => 'nullable|string|max:255',
+            'page' => 'nullable|int'
+        ];
+    }
+}
